d4e720b37184b2ef5ab1ef8d70f8022781f26398
BenLirio <benjlirio@gmail.com>
2022-04-08 14:42:36 -0400

    add semi colons

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-let width = 400
-let height = 400
+/*global
+  createCanvas,
+  background,
+*/
+// Add functions to this list that give you a red dot.
+
+var width = 400;
+var height = 400;
 
 function setup() {
-  createCanvas(width, height)
+  createCanvas(width, height);
 }
 
 function draw() {
-  background(200)
+  background(200);
 }
